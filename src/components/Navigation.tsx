0df68ade7a2719b31abf4d84bada14e73a1ff59e
Rajesh Rajesh <94539948+rajeshpotlia1@users.noreply.github.com>
2026-07-20 14:51:43 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,7 +9,7 @@ const NAV_LINKS = [
   { label: "About", target: "about" },
   { label: "Research", target: "research" },
   { label: "Publications", target: "publications" },
-  { label: "News", target: "news-updates" },
+  { label: "News", target: "community" },
   { label: "Contact", target: "contact" },
 ];
 
